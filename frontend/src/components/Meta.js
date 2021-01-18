1d518b6ac9a87523b15a8ff99a37771d56ecdcfc
gavin7gomes <gavinrockgomes@gmail.com>
2021-01-19 01:08:49 +0530

    Initial commit

---
+++ frontend/src/components/Meta.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const Meta = ({ title, description, keywords }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name='description' content={description} />
+      <meta name='keyword' content={keywords} />
+    </Helmet>
+  );
+};
+
+Meta.defaultProps = {
+  title: 'Welcome To Gomes Garden',
+  description: 'We sell best organic products for cheap',
+  keywords: 'vegetables, fruits, organic, farm, cheap, healthy food',
+};
+
+export default Meta;
